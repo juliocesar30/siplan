66e218ed5dbff363a06ec8868afd095081fc0945
martinsillo <martinsillo.gamboa@gmail.com>
2016-08-09 14:12:39 -0500

    se modifica encabezado, se comentan modulos de comentarios y mensajes

--- views/estructura/cabecera.php
+++ views/estructura/cabecera.php
@@ -3,12 +3,9 @@
 <nav class="navbar navbar-static-top  " role="navigation" style="margin-bottom: 0">
 <div class="navbar-header">
 <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
-
 </div>
-            <ul class="nav navbar-top-links navbar-right">
-                <li>
-                    <span class="m-r-sm text-muted welcome-message">Welcome to INSPINIA+ Admin Theme.</span>
-                </li>
+<ul class="nav navbar-top-links navbar-right">
+    <?php /*
                 <li class="dropdown">
                     <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
                         <i class="fa fa-envelope"></i>  <span class="label label-warning">16</span>
@@ -195,13 +192,13 @@
 
 
 
-
+*/ ?>
 
                 <li>
 
-                    <a href="login.html">
+                    <a href="index.php">
 
-                        <i class="fa fa-sign-out"></i> Log out
+                        <i class="fa fa-sign-out"></i> Salir
 
                     </a>
 
