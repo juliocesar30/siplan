ed0d5eee1499a1b7afeb92bb03d99f44fa0b9664
martinsillo <martinsillo.gamboa@gmail.com>
2016-08-09 14:12:46 -0500

    new file

---
+++ views/estructura/footer.php
@@ -0,0 +1,42 @@
+
+<div class="footer">
+<div class="pull-right">
+                    Gobierno del Estado de Zacatecas <strong>2016 - 2021</strong>
+                </div>
+
+                <div>
+
+                    <strong>UPLA</strong>
+
+                </div>
+
+            </div>
+
+
+
+        </div>
+
+        </div>
+
+
+
+    <!-- Mainly scripts -->
+
+    <script src="js/jquery-2.1.1.js"></script>
+
+    <script src="js/bootstrap.min.js"></script>
+
+    <script src="js/plugins/jquery.metisMenu.js"></script>
+
+    <script src="js/plugins/jquery.slimscroll.min.js"></script>
+
+
+
+    <!-- Custom and plugin javascript -->
+
+    <script src="js/inspinia.js"></script>
+
+    <script src="js/plugins/pace.min.js"></script>
+
+
+
