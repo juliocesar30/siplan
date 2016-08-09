26e9310d48c6da4f6811fe679a0ebcfa9b3ab6b3
martinsillo <martinsillo.gamboa@gmail.com>
2016-08-09 14:12:15 -0500

    se agregan contenido y footer, se limpia codigo sobrante

--- main.php
+++ main.php
@@ -12,7 +12,6 @@ if($_SESSION['id_perfil'] == 1){
 }else{
   $body_class = "pace-down skin-1";
 }
-
 ?>
 <body class="<?php echo $body_class;?>">
 <div id="wrapper">
@@ -20,80 +19,8 @@ if($_SESSION['id_perfil'] == 1){
 <div id="page-wrapper" class="gray-bg">
 <?php require_once("views/estructura/cabecera.php"); ?>
 <?php require_once("class/contenidos.php"); ?>
-
-
-
-
-            <div class="wrapper wrapper-content">
-
-                <div class="middle-box text-center animated fadeInRightBig">
-
-                    <h3 class="font-bold">This is page content</h3>
-
-                    <div class="error-desc">
-
-                        You can create here any grid layout you want. And any variation layout you imagine:) Check out
-
-                        main dashboard and other site. It use many different layout.
-
-                        <br/><a href="index.html" class="btn btn-primary m-t">Dashboard</a>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-            <div class="footer">
-
-                <div class="pull-right">
-
-                    10GB of <strong>250GB</strong> Free.
-
-                </div>
-
-                <div>
-
-                    <strong>Copyright</strong> Example Company &copy; 2014-2015
-
-                </div>
-
-            </div>
-
-
-
-        </div>
-
-        </div>
-
-
-
-    <!-- Mainly scripts -->
-
-    <script src="js/jquery-2.1.1.js"></script>
-
-    <script src="js/bootstrap.min.js"></script>
-
-    <script src="js/plugins/jquery.metisMenu.js"></script>
-
-    <script src="js/plugins/jquery.slimscroll.min.js"></script>
-
-
-
-    <!-- Custom and plugin javascript -->
-
-    <script src="js/inspinia.js"></script>
-
-    <script src="js/plugins/pace.min.js"></script>
-
-
-
-
-
+<?php require_once("views/estructura/footer.php"); ?>
 </body>
-
-
-
 </html>
 
 
