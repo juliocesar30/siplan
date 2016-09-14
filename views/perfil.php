3d993901172274da5040e723f6f5fcd61a034eb3
José Martín Gamboa Murillo <martinsillo.gamboa@gmail.com>
2016-09-14 12:51:18 -0500

    new file

---
+++ views/perfil.php
@@ -0,0 +1,32 @@
+<div class="row wrapper border-bottom white-bg page-heading">
+<div class="col-lg-10">
+<h2>Perfil de Usuario</h2>
+</div>
+</div>
+<div class="wrapper wrapper-content animated fadeInRight">
+<div class="row">
+<div class="col-lg-12">
+<div class="ibox float-e-margins">
+<div class="ibox-title">
+<h5>Ver/Editar información del usuario</h5>
+</div>
+<div class="ibox-content">
+<form method="get" class="form-horizontal">
+<div class="row">
+<div class="col-lg-2">
+<div class="profile-image">
+<img src="images/avatares/img01.png" class="img-circle circle-border m-b-md" alt="profile">
+<br><a href="#">Editar Foto de Perfil</a>
+</div>
+
+</div>
+<div class="col-lg-10">
+    <label for="usuario">Usuario: </label>
+    <input type="text" disabled value="usuario">
+    <br>
+    <label for="usuario">Clave: </label>
+    <input type="password" disabled value="usuario">
+</div>
+</div>
+</form>
+</div></div></div></div></div>
